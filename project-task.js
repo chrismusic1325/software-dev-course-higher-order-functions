02337fc84735c0b79a91f565d367962b880e053d
Chris Haefner <chrismusic1325@gmail.com>
2026-05-23 15:59:03 -0400

    Fixed higher order functions assignment

--- project-task.js
+++ project-task.js
@@ -6,12 +6,10 @@ const products = [
   { name: "Keyboard", price: 100, inStock: false },
 ];
 
-// 1. Higher-Order Function for Filtering
 function filterProducts(productsArray, callback) {
   return productsArray.filter(callback);
 }
 
-// Filter products in stock
 const inStockProducts = filterProducts(
   products,
   product => product.inStock
@@ -20,7 +18,6 @@ const inStockProducts = filterProducts(
 console.log("In Stock Products:");
 console.log(inStockProducts);
 
-// Filter expensive products
 const expensiveProducts = filterProducts(
   products,
   product => product.price > 500
@@ -29,7 +26,6 @@ const expensiveProducts = filterProducts(
 console.log("Products Above $500:");
 console.log(expensiveProducts);
 
-// 2. Transform Product Names
 const uppercaseNames = products.map(product =>
   product.name.toUpperCase()
 );
@@ -37,7 +33,6 @@ const uppercaseNames = products.map(product =>
 console.log("Uppercase Product Names:");
 console.log(uppercaseNames);
 
-// 3. Higher-Order Function Returning a Function
 function applyDiscount(discountPercent) {
   return function(price) {
     return price - (price * discountPercent / 100);
@@ -56,73 +51,6 @@ const discountedProducts = products.map(product => {
 console.log("Discounted Products:");
 console.log(discountedProducts);
 
-// 4. Total Inventory Value
-const totalInventoryValue = products
-  .filter(product => product.inStock)
-  .reduce((total, product) => total + product.price, 0);
-
-console.log("Total Inventory Value:");
-console.log(totalInventoryValue);
-EOFcat > project-task.js <<'EOF'
-const products = [
-  { name: "Laptop", price: 1000, inStock: true },
-  { name: "Phone", price: 500, inStock: false },
-  { name: "Tablet", price: 800, inStock: true },
-  { name: "Monitor", price: 300, inStock: true },
-  { name: "Keyboard", price: 100, inStock: false },
-];
-
-// 1. Higher-Order Function for Filtering
-function filterProducts(productsArray, callback) {
-  return productsArray.filter(callback);
-}
-
-// Filter products in stock
-const inStockProducts = filterProducts(
-  products,
-  product => product.inStock
-);
-
-console.log("In Stock Products:");
-console.log(inStockProducts);
-
-// Filter expensive products
-const expensiveProducts = filterProducts(
-  products,
-  product => product.price > 500
-);
-
-console.log("Products Above $500:");
-console.log(expensiveProducts);
-
-// 2. Transform Product Names
-const uppercaseNames = products.map(product =>
-  product.name.toUpperCase()
-);
-
-console.log("Uppercase Product Names:");
-console.log(uppercaseNames);
-
-// 3. Higher-Order Function Returning a Function
-function applyDiscount(discountPercent) {
-  return function(price) {
-    return price - (price * discountPercent / 100);
-  };
-}
-
-const discount20 = applyDiscount(20);
-
-const discountedProducts = products.map(product => {
-  return {
-    name: product.name,
-    discountedPrice: discount20(product.price)
-  };
-});
-
-console.log("Discounted Products:");
-console.log(discountedProducts);
-
-// 4. Total Inventory Value
 const totalInventoryValue = products
   .filter(product => product.inStock)
   .reduce((total, product) => total + product.price, 0);
